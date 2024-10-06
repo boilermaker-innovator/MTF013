73a9c40486b8e428acd6ce2358c3fa478c4e9055
boilermaker-innovator <jgwynne7@gmail.com>
2024-10-06 21:05:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    var viewTeaserListBtn = document.getElementById('viewTeaserListBtn');
+    if (viewTeaserListBtn) {
+        viewTeaserListBtn.addEventListener('click', function() {
+            window.location.href = 'teaser-list.html';
+        });
+    }
+});
